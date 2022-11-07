aa47fc0f687ad6ac2125a72cffe2adf15194bd64
Joseph Cox <73198556+mapleweekend@users.noreply.github.com>
2022-11-06 20:14:23 -0500

    Update main.js

--- dist/scripts/frontend/main.js
+++ dist/scripts/frontend/main.js
@@ -1,8 +1,8 @@
 import { newItem, loadItems } from '/dist/scripts/frontend/itemManager.js';
 import { getPage } from '/dist/scripts/frontend/page.js';
 import { init_login } from '/dist/scripts/frontend/auth.js';
-// import { addFriend } from '/dist/scripts/frontend/friends.js';
-// import { loadNotifications } from '/dist/scripts/frontend/notifications.js';
+import { addFriend } from '/dist/scripts/frontend/friends.js';
+import { loadNotifications } from '/dist/scripts/frontend/notifications.js';
 
 export const qr_base = "https://chart.googleapis.com/chart?cht=qr&chs=180x180&choe=UTF-8&chl=";
 
