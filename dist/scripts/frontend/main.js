55e9961484dfab27e2ccc3bc469e88a9a60c349e
Joseph Cox <73198556+mapleweekend@users.noreply.github.com>
2022-11-06 20:11:50 -0500

    testing routing issues

--- dist/scripts/frontend/main.js
+++ dist/scripts/frontend/main.js
@@ -1,8 +1,8 @@
 import { newItem, loadItems } from '/dist/scripts/frontend/itemManager.js';
 import { getPage } from '/dist/scripts/frontend/page.js';
 import { init_login } from '/dist/scripts/frontend/auth.js';
-import { addFriend } from '/dist/scripts/frontend/friends.js';
-import { loadNotifications } from '/dist/scripts/frontend/notifications.js';
+// import { addFriend } from '/dist/scripts/frontend/friends.js';
+// import { loadNotifications } from '/dist/scripts/frontend/notifications.js';
 
 export const qr_base = "https://chart.googleapis.com/chart?cht=qr&chs=180x180&choe=UTF-8&chl=";
 
@@ -10,12 +10,12 @@ window.onload = () => {
     console.log("window loaded")
     init_login();
     if (getPage() == '/') {
-        const submit_newitem = document.getElementById("submit_newitem");
-        submit_newitem.addEventListener("click", newItem);
-        const submit_addfriend = document.getElementById("submit_addfriend");
-        submit_addfriend.addEventListener("click", addFriend);
+        // const submit_newitem = document.getElementById("submit_newitem");
+        // submit_newitem.addEventListener("click", newItem);
+        // const submit_addfriend = document.getElementById("submit_addfriend");
+        // submit_addfriend.addEventListener("click", addFriend);
 
         loadItems();
     }
-    loadNotifications();
+    // loadNotifications();
 }
